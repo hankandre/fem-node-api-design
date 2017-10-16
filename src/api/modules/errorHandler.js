9556b453803c79200a274d255fb0be96a59cf0fd
Hank Andre <hankandre@gmail.com>
2017-10-16 14:17:26 -0500

    edited error handler

--- src/api/modules/errorHandler.js
+++ src/api/modules/errorHandler.js
@@ -8,11 +8,10 @@ export const apiErrorHandler = (error, req, res, next) => {
   return writeFile(resolve(__dirname, '../../../error.log'), error.stack, {
     encoding: 'utf8'
   })
-    .then(success => {
-      console.log('success!', success);
-      res.status(502).json({ error: error.message });
+    .then(err => {
+      res.status(502).json({ error: error.message || err });
     })
-    .catch(err => console.error(err));
+    .catch(err => res.status(502).json({ error: err }));
   // console.log(error.stack);
   // res.status(502).json({ error: error.message } || JSON.stringify(error));
 };
