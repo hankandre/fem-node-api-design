eaf476b5b3dd84111978ad6f8aff1f4597b5ea6f
Hank Andre <hankandre@gmail.com>
2017-10-16 10:26:47 -0500

    Removed brackets

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import express from 'express';
 // Declare an app from express
 const app = express();
 
-app.all('*', (req, res) => {
-  res.json({ ok: true });
-});
+app.all('*', (req, res) => res.json({ ok: true }));
 
 export default app;
