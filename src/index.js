7f6c1c6cb77b327f78696fa6c145e739200d8531
Hank Andre <hankandre@gmail.com>
2017-10-16 10:24:23 -0500

    Lesson 1 complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import http from 'http'
-import { createServer } from 'http'
+import http from 'http';
 
-import app from './server'
-let currentApp = app
+import app from './server';
 
+const server = http.createServer(app);
+let currentApp = app;
+
+const service = server.listen(3000, () => {
+  console.log('listening on port %d', service.address().port);
+});
 
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(['./server'], () => {
+    server.removeListener('request', currentApp);
+    server.on('request', app);
+    currentApp = app;
+  });
 }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
-// Declare an app from express
-const app = null
+import express from 'express';
 
-export default app
+// Declare an app from express
+const app = express();
+
+app.all('*', (req, res) => {
+  res.json({ ok: true });
+});
+
+export default app;
